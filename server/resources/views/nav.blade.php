a0eb0375529f89a379eb3c22dbeb3cfc93f6c584
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-07-10 18:32:15 +0900

    2-6 ログアウトを可能にする

--- server/resources/views/nav.blade.php
+++ server/resources/views/nav.blade.php
@@ -4,35 +4,45 @@
 
     <ul class="navbar-nav ml-auto">
 
-        <li class="nav-item">
-            <a href="" class="nav-link">ユーザー登録</a>
-        </li>
+        @guest
+            <li class="nav-item">
+                <a href="{{ route('register') }}" class="nav-link">ユーザー登録</a>
+            </li>
+        @endguest
 
-        <li class="nav-item">
-            <a href="" class="nav-link">ログイン</a>
-        </li>
-        <li class="nav-item">
-            <a href="" class="nav-link"><i class="fas fa-pen mr-1"></i>投稿する</a>
-        </li>
+        @guest
+            <li class="nav-item">
+                <a href="" class="nav-link">ログイン</a>
+            </li>
+        @endguest
 
-        {{-- Dropdown --}}
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
-                aria-expanded="false">
-                <i class="fas fa-user-circle"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                <button class="dropdown-item type="button" onclick="location.href="">
-                    マイページ
-                </button>
-                <div class="dropdown-divider"></div>
-                <button form="logout-button" class="dropdown-item" type="submit">
-                    ログアウト
-                </button>
-            </div>
-        </li>
-        <form action="" id="logout-button" method="POST">
-        </form>
-        {{-- Dropdown --}}
+        @auth
+            <li class="nav-item">
+                <a href="" class="nav-link"><i class="fas fa-pen mr-1"></i>投稿する</a>
+            </li>
+        @endauth
+
+        @auth
+            {{-- Dropdown --}}
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <i class="fas fa-user-circle"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <button class="dropdown-item type="button" onclick="location.href="">
+                        マイページ
+                    </button>
+                    <div class="dropdown-divider"></div>
+                    <button form="logout-button" class="dropdown-item" type="submit">
+                        ログアウト
+                    </button>
+                </div>
+            </li>
+            <form action="{{ route('logout') }}" id="logout-button" method="POST">
+                @csrf
+            </form>
+            {{-- Dropdown --}}
+        @endauth
     </ul>
 </nav>
