9261fad364f64e1a4c14a08673efe896a26c0cf7
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-07-11 18:15:43 +0900

    5-2 記事更新画面と記事削除モーダル画面の表削除モーダル画面の表示

--- server/resources/views/articles/form.blade.php
+++ server/resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
     <label>タイトル</label>
-    <input type="text" name="title" class="form-control" value="{{ old('title') }}" required>
+    <input type="text" name="title" class="form-control" value="{{ $article->title ?? old('title') }}" required>
 </div>
 <div class="form-group">
     <label></label>
-    <textarea name="body" class="form-control" rows="16" placeholder="本文" required>{{ old('body') }}</textarea>
+    <textarea name="body" class="form-control" rows="16" placeholder="本文" required>{{ $article->body ?? old('body') }}</textarea>
 </div>
