5404c5bc5ce9ebdb2c0bd8c4189149c0996ff0e8
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-07-19 18:37:36 +0900

    8-2 タグ入力のVueコンポーネントを表示する

--- server/resources/views/articles/form.blade.php
+++ server/resources/views/articles/form.blade.php
@@ -4,6 +4,10 @@
     <input type="text" name="title" class="form-control" value="{{ $article->title ?? old('title') }}" required>
 </div>
 <div class="form-group">
+    <article-tags-input>
+    </article-tags-input>
+</div>
+<div class="form-group">
     <label></label>
     <textarea name="body" class="form-control" rows="16" placeholder="本文" required>{{ $article->body ?? old('body') }}</textarea>
 </div>
