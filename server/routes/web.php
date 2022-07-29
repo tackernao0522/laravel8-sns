e5eb99dbf315d56a52a71772fa00b034e3b1dff6
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-07-29 16:28:15 +0900

    10-7 Googleのアカウントでユーザー登録可能にする

--- server/routes/web.php
+++ server/routes/web.php
@@ -15,6 +15,7 @@ Route::prefix('login')->name('login.')->group(function () {
 });
 Route::prefix('register')->name('register.')->group(function () {
   Route::get('/{provider}', [RegisterController::class, 'showProviderUserRegistrationForm'])->name('{provider}');
+  Route::post('/{provider}', [RegisterController::class, 'registerProviderUser'])->name('{provider}');
 });
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 Route::resource('/articles', ArticleController::class)->except(['index', 'show'])->middleware('auth');
